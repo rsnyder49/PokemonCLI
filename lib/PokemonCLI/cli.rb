b2747130cfc73c089afa6b3e3adac216862535e1
Robert Snyder <r.snyder49@gmail.com>
2018-09-18 21:39:07 +0000

    modified bin file

--- lib/PokemonCLI/cli.rb
+++ lib/PokemonCLI/cli.rb
@@ -3,7 +3,11 @@ class PokemonCLI::CLI
   
   def start 
     puts "Welcome to the PokemonCLI object creator"
-    
+    list_pokemon
+  end 
+  
+  def list_pokemon
+    puts "1. Bulbasaur"
   end 
   
 end 
--- lib/PokemonCLI/scraper.rb
+++ lib/PokemonCLI/scraper.rb
@@ -1,4 +1,6 @@
-class PokemonCLI::Scraper
+require 'pry'
+
+class Scraper
   
   #method will scrape data from a url. will add name and num attributes
   def get_page
@@ -8,7 +10,10 @@ class PokemonCLI::Scraper
     poke_dex_array = []
     
     poke_dex.css("div grid-col span-md-12 span-lg-10").each do |pokemon| 
-      
+      poke_dex_array << {
+        :number => pokemon.css("span infocard-cell-data").text
+      }
+      binding.pry
     end
       
   end 
