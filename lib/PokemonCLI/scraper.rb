6cc7ee9f9c19423d5df345d51844588af108dc29
Robert Snyder <r.snyder49@gmail.com>
2018-09-20 01:10:27 +0000

    modified scraper.rb

--- lib/PokemonCLI/scraper.rb
+++ lib/PokemonCLI/scraper.rb
@@ -13,21 +13,20 @@ class Scraper
   def get_all_page
     # html = File.read("https://pokemondb.net/pokedex/all")
     # poke_dex = Nokogiri::HTML(html)
-    # doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/all"))
-    doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/national"))
+    doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex/all"))
     doc
     
   end 
   
   def add_num_and_name
-    poke_dex_hash = {}
+    poke_dex = []
     
-    get_all_page.css("tbody").each do |pokemon| 
-    #   poke_dex_hash[:number] = pokemon.css("span.infocard-cell-data").text
-      poke_dex_hash[:name] = pokemon.css("tr td.cell-name a.ent-name").text
+    get_all_page.css("tr").each do |pokemon| 
+      poke_dex << pokemon.css("span.infocard-cell-data").text
+      poke_dex << pokemon.css("tr td.cell-name").text
       
     end
-    poke_dex_hash
+    poke_dex.reject{|a| a.empty?}
   end
   binding.pry
   def self.get_pokemon_page
