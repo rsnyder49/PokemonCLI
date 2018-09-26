922585b444165ad367931838df269ffa35598f2f
Robert Snyder <r.snyder49@gmail.com>
2018-09-26 19:09:35 +0000

    modified cli.rb

--- lib/PokemonCLI/cli.rb
+++ lib/PokemonCLI/cli.rb
@@ -1,55 +1,76 @@
 class PokemonCLI::CLI  
-  attr_accessor :name
+  #attr_accessor :name
   def start 
-    input = ""
     puts "Welcome to the PokemonCLI!"
-    puts "Input '1' to view Pokemon numbers 1 to 250."
-    puts "Input '2' to view Pokemon numbers 251 to 500."
-    puts "Input '3' to view Pokemon numbers 501 to 807."
-    
-    input = gets.strip 
-    case input 
-    when 1 
-    
+    list_pokemon
     menu
+    good_bye
+  end
+  
+  def list_pokemon 
+    input = ""
+    # while input != 'exit'
+      puts "Input '1' to view Pokemon numbers 1 to 250."
+      puts "Input '2' to view Pokemon numbers 251 to 500."
+      puts "Input '3' to view Pokemon numbers 501 to 807 or type 'exit' to close the program."
+      input = gets.downcase.strip 
+      case input.to_i  
+      when 1 
+        list_pokemon_1_to_250 
+        #menu
+      when 2 
+        list_pokemon_251_to_500
+        #menu
+      when 3 
+        list_pokemon_501_to_807
+        #menu
+      when 'exit'
+        good_bye
+      else 
+        "I did not understand that unput"
+      #end
+    end
   end 
   
   def list_pokemon_1_to_250
     PokemonCLI::Pokemon.create_all
-    @all_pokemon = PokemonCLI::Pokemon.all_pokemon[1..250]
-    @all_pokemon.each do |pokemon|
+    all_pokemon = PokemonCLI::Pokemon.all_pokemon[1..250]
+    all_pokemon.each do |pokemon|
       puts "#{pokemon.number}. #{pokemon.name}"
     end
   end 
   
   def list_pokemon_251_to_500 
     PokemonCLI::Pokemon.create_all
-    @all_pokemon = PokemonCLI::Pokemon.all_pokemon[251..500]
-    @all_pokemon.each do |pokemon|
+    all_pokemon = PokemonCLI::Pokemon.all_pokemon[251..500]
+    all_pokemon.each do |pokemon|
       puts "#{pokemon.number}. #{pokemon.name}"
     end 
   end 
   
   def list_pokemon_501_to_807
     PokemonCLI::Pokemon.create_all
-    @all_pokemon = PokemonCLI::Pokemon.all_pokemon[501..807]
-    @all_pokemon.each do |pokemon|
+    all_pokemon = PokemonCLI::Pokemon.all_pokemon[501..807]
+    all_pokemon.each do |pokemon|
       puts "#{pokemon.number}. #{pokemon.name}"
     end 
   end 
   
+  # def get_all_pokemon
+  #   PokemonCLI::Pokemon.create_all
+  #   @all_pokemon = PokemonCLI::Pokemon.all_pokemon
+  # end 
+  
   def menu
     input = ""
+    PokemonCLI::Pokemon.create_all
+    @all_pokemon = PokemonCLI::Pokemon.all_pokemon
     while input != 'exit'
-    
       puts "Please enter a Pokemon number to see additional information about that Pokemon or type 'list' to view the list of Pokemon or type 'exit' to close the program."
-      
       input = gets.downcase.strip
-      
       if input.to_i > 0 
-        current_pokemon = @all_pokemon[input.to_i - 1] 
+        current_pokemon = @all_pokemon[input.to_i] 
         pokemon = PokemonCLI::Pokemon.get_attributes(current_pokemon.name)
-        
         puts pokemon.name
         puts "Num: #{pokemon.number}"
         puts "Type: #{pokemon.type}"
@@ -58,11 +79,10 @@ class PokemonCLI::CLI
         puts "Weight: #{pokemon.weight}"
         puts "Abilities: #{pokemon.abilities}"
         puts "-----------------------"
-        
       elsif input == "list"
         list_pokemon
       elsif input == 'exit' 
-        good_bye
+      #   good_bye
       else
         puts "I did not understand that input."
       end 
